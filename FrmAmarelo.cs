3017e9a1a2f0b692a3653d2bfd21cc3ed8a89a6c
Arthur Miranda <12001830@aluno.cotemig.com.br>
2022-05-11 14:25:35 -0300

    Resumo do projeto

---
+++ FrmAmarelo.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AppExemploMenu
+{
+    public partial class FrmAmarelo : Form
+    {
+        public FrmAmarelo()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmAmarelo_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void exibirFormAmarelo_Click(object sender, EventArgs e)
+        {
+            FrmAmarelo frm = new FrmAmarelo(); //Indico qual form será aberto
+            this.Close(); //fechar o atual
+            frm.ShowDialog();
+        }
+
+        private void exibirFormVermelho_Click(object sender, EventArgs e)
+        {
+            FrmVermelho frm = new FrmVermelho(); //Indico qual form será aberto
+            this.Close(); //fechar o atual
+            frm.ShowDialog();
+        }
+
+        private void exibirFormAzul_Click(object sender, EventArgs e)
+        {
+            FrmAzul frm = new FrmAzul(); //Indico qual form será aberto
+            this.Close(); //fechar o atual
+            frm.ShowDialog();
+        }
+    }
+}
